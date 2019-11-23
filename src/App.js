c9aa94175d19fac33ad3b1b2791e74a6ae27bc1b
markjanzer <mark.janzer@gmail.com>
2019-11-22 17:38:36 -0800

    Setup rough draft

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
+import Select from 'react-select';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.filterOptions = [
+      { value: 'chocolate', label: 'Chocolate' },
+      { value: 'strawberry', label: 'Strawberry' },
+      { value: 'vanilla', label: 'Vanilla' },
+    ];
+
+    this.sortOptions = [
+      { value: 'none', label: 'none' },
+      { value: 'alphabetical', label: 'alphabetical' },
+      { value: 'reverse alphabetical', label: 'reverse alphabetical' },
+    ]
+
+    this.iceCreams = [
+      { id: 1, flavor: 'chocolate' },
+      { id: 2, flavor: 'strawberry' },
+      { id: 3, flavor: 'vanilla' },
+    ]
+
+    this.state = {
+      filter: this.filterOptions[0],
+      sort: this.sortOptions[0],
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <nav>
+          <Select
+            value={this.state.filter}
+            onChange={(value) => this.setState({ filter: value })}
+            options={this.filterOptions}
+          />
+          <Select
+            value={this.state.sort}
+            onChange={(value) => this.setState({ sort: value })}
+            options={this.sortOptions}
+          />
+        </nav>
+        <ul>
+          {this.iceCreams.map(iceCream => {
+            return (
+              <li
+                key={iceCream.id}
+              >{iceCream.flavor}</li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
